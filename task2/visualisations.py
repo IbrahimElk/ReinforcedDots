572ac9fbb57532f5196d7a70a5e8d32e5b29d0cf
StafRys <staf.rys@student.kuleuven.be>
2024-03-19 18:45:51 +0100

    Added remaining games

--- task2/visualisations.py
+++ task2/visualisations.py
@@ -14,17 +14,22 @@ def main():
     Generates the requiered visualisations for the 4 requested matrix games
     """
     game_sub = pyspiel.create_matrix_game("subsidy_game", "Subsidy Game", ["S1","S2"], ["S1","S2"],
-                                      [[12,0],[11,10]], [[12,11],[0,10]])
+                                            [[12,0],[11,10]], [[12,11],[0,10]])
     game_rps = pyspiel.load_matrix_game("matrix_rps")
+    game_bos = pyspiel.create_matrix_game("battle_of_the_sexes", "Battle of the Sexes", ["O","M"],["O","M"],
+                                            [[3,0],[0,2]],[[2,0],[0,3]])
+    game_pd = pyspiel.create_matrix_game("prisoners_dilemma", "Prisoners Dilemma", ["C","D"],["C","D"],
+                                            [[-1,-4],[0,-3]],[[-1,0],[-4,-3]])
 
     ## TODO add other matrix games above.
-
     plot_replicator_dynamics_2x2(game_sub)
     plot_replicator_dynamics_rps(game_rps)
+    plot_replicator_dynamics_2x2(game_bos)
+    plot_replicator_dynamics_2x2(game_pd)
 
     
 
-def plot_replicator_dynamics_rps(game, history = None):
+def plot_replicator_dynamics_rps(game):
     current_directory_path = os.getcwd()
     subfolder_path = os.path.join(current_directory_path,'task2','images')
     if not os.path.exists(subfolder_path):
@@ -34,16 +39,22 @@ def plot_replicator_dynamics_rps(game, history = None):
     payoff_tensor = utils.game_payoffs_array(game)
     dyn = dynamics.SinglePopulationDynamics(payoff_tensor,dynamics.replicator)
 
+    # Stream plot
     fig = plt.figure(figsize=(4,4))
     rpdyn = fig.add_subplot(111, projection="3x3")
-    rpdyn.quiver(dyn)
+    rpdyn.streamplot(dyn,density=0.75,color='black')
+    
+    # Quiver plot
+    rpdyn.quiver(dyn,color='blue')
+    
+    
     rpdyn.set_labels(["Rock", "Paper", "Scissors"])
-    rpdyn.set_title(f"{game.get_type().long_name} trajectory plot")
-    filepath = os.path.join(subfolder_path,"traj_plot_" + game.get_type().short_name +".png")
+    rpdyn.set_title(f"Directional field plot: {game.get_type().long_name}")
+    filepath = os.path.join(subfolder_path,"dir_field_plot_" + game.get_type().short_name +".png")
     plt.savefig(filepath)
     plt.show()
 
-def plot_replicator_dynamics_2x2(game,history=None):
+def plot_replicator_dynamics_2x2(game):
     current_directory_path = os.getcwd()
     subfolder_path = os.path.join(current_directory_path,'task2','images')
     if not os.path.exists(subfolder_path):
@@ -53,15 +64,25 @@ def plot_replicator_dynamics_2x2(game,history=None):
     payoff_tensor = utils.game_payoffs_array(game)
     dyn = dynamics.MultiPopulationDynamics(payoff_tensor,dynamics.replicator)
 
+    # Stream plot
     fig = plt.figure(figsize=(4,4))
     rpdyn = fig.add_subplot(111, projection="2x2")
-    rpdyn.streamplot(dyn)
+    rpdyn.streamplot(dyn,density=0.75,color='black')
+
+    # Quiver plot
+    rpdyn.quiver(dyn,color='blue')
+    #if history:
+    #    for pop_hist in history:
+    #        x = [hist[0][0] for hist in pop_hist]
+    #        y = [hist[1][0] for hist in pop_hist]
+    
+    
     plt.xlim(-0.01,1.01)
     plt.ylim(-0.01,1.01)
     plt.xlabel(f"P({game.row_action_name(0)}) Agent 1")
     plt.ylabel(f"P({game.row_action_name(0)}) Agent 2")
-    rpdyn.set_title(f"{game.get_type().long_name} trajectory plot")
-    filepath = os.path.join(subfolder_path,"traj_plot_" + game.get_type().short_name +".png")
+    rpdyn.set_title(f"Directional field plot: {game.get_type().long_name}")
+    filepath = os.path.join(subfolder_path,"dir_field_plot_" + game.get_type().short_name +".png")
     plt.savefig(filepath)
     plt.show()
 
