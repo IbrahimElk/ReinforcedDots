b7543075d5747404e4563496772924ad0b8de7c9
IbrahimElk <ibrahim.elkaddouri@student.kuleuven.be>
2024-04-20 13:26:03 +0200

    [MOD] change own hashing function to dbn.tostring

---
+++ task3/evaluation.py
@@ -0,0 +1,4 @@
+# Use these the three optimizations (transposition table, symmetry and chains)
+# and compare with the provided template algorithm. For various game board
+# sizes, plot both the execution times and the number of keys.
+
