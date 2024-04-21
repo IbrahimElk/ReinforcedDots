8a097953017cbf9c51e5beb9f62b39ba78fb4817
IbrahimElk <ibrahim.elkaddouri@student.kuleuven.be>
2024-04-21 09:43:23 +0200

    [MOD] relocate minimax algorithms to subfolder

--- task3/mixed_minimax.py
+++
@@ -1,7 +0,0 @@
-import chains_minimax
-import symmetry_minimax
-import transpositon_minimax
-
-def minimax_mixed_search():
-    """ functie met alles der op en der aan (van optimisaties) """
-    pass
--- task3/symmetry_minimax.py
+++
@@ -1,2 +0,0 @@
-def minimax_symmetry_search():
-    pass
