b6e829ee294a17b78914b838fc2642ba626ec7ca
StafRys <staf.rys@student.kuleuven.be>
2024-04-29 17:25:11 +0200

    Long chains strat minimax with depth limitation

--- task3/chains.py
+++ task3/chains.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 Chains are sequences of one or more capturable boxes ("corridors").
 
@@ -21,15 +23,135 @@ Closed chains, only 2 moves is part of a optimal strategy:
 More than 1 chain available:
     1. Fill all but 1 available chains, and follow the above mentioned strategies for the remaining
         chain. If possible choose half-open chain as the last one. (Sacrifices only 2 instead of 4)
+
+        
+
+Stackoverflow:
+
+The first is the double-cross strategy: The same double-cross strategy applies no 
+matter how many long chains there are: a player using this strategy will take all 
+but two boxes in each chain and take all the boxes in the last chain. If the chains 
+are long enough, then this player will win. 
+
+Second, the parity rule for long chains. This follows from three facts about the majority of well-played games:
+
+    1.The long chains will be played out at the very end of the game.
+    2.There will be a double cross in every chain except the last one.
+    3.The player who first has to play in any long chain loses the game.
+
+    
+The third and most powerful observation is that dots and boxes is an impartial game: 
+the available moves are the same regardless of whose turn it is to play, and in typical 
+positions that arise in the course of play (that is, ones containing long chains of boxes) 
+it's also a normal game: the last player to move wins. The combination of these properties 
+means that positions can be analyzed statically using Sprague–Grundy theory.
+"""
+
+"""
+Max player goes first, min player goes second.
+
+For board m x n (boxes):
+    if m and n are both even:
+        max: amt long chains odd
+    else:
+        max: amt long chains even
 """
 import pyspiel
 from absl import app
 
+def game_state_tensor(state):
+    state_info = state.observation_tensor(0)
+    num_rows = state.get_game().get_parameters()['num_rows']
+    num_cols = state.get_game().get_parameters()['num_cols']
+    num_dots= ((1 + num_rows) * (1 + num_cols))
 
-def _minimax(state, maximizing_player_id):
+    np_state_info = np.array(state_info)
+    state_matrix = np_state_info.reshape(3, num_dots, 3)
+    
+    return state_matrix
+
+
+def pvector_for_tensor(tensor):
+    p = np.zeros(len(tensor[0]))
+
+    # Player 1
+    for i in range(len(tensor[1])):
+        if tensor[1][i][2] == 1:
+            p[i] = 1
+
+    # Player 2
+    for i in range(len(tensor[2])):
+        if tensor[2][i][2] == 1:
+            p[i] = 2
+ 
+    return p
+
+def vvector_for_tensor(tensor):
+    v = np.zeros(len(tensor[0]))
+
+    # Player 1
+    for i in range(len(tensor[1])):
+        if tensor[1][i][1] == 1:
+            v[i] = 1
+
+    # Player 2
+    for i in range(len(tensor[2])):
+        if tensor[2][i][1] == 1:
+            v[i] = 2
+ 
+    return v
+
+def hvector_for_tensor(tensor):
+    h = np.zeros(len(tensor[0]))
+
+    # Player 1
+    for i in range(len(tensor[1])):
+        if tensor[1][i][0] == 1:
+            h[i] = 1
+
+    # Player 2
+    for i in range(len(tensor[2])):
+        if tensor[2][i][0] == 1:
+            h[i] = 2
+ 
+    return h
+
+def box_state_for_pvector(pvector, ncols):
+    result = np.delete(pvector,np.arange(2,len(pvector),3))
+    result = np.delete(result,np.arange(len(result)-ncols,len(result)))
+    return result
+
+def box_to_dot(boxnum, state):
+    """
+    Returns the dot number of the upper left corner of the given box number.
+    """
+    num_cols = state.get_game().get_parameters()['num_cols']
+
+    row = boxnum // num_cols
+    col = boxnum % num_cols
+
+    dot = row * (num_cols + 1) + col
+    return dot
+
+def is_vedge_present(state,v_vector, box1, box2):
+    """
+    Checks if there is a vertical edge present between given boxes.
+    """
+    i = max(box1, box2)
+    return v_vector[box_to_dot(i,state)] != 0
+
+
+def is_hedge_present(state,h_vector, box1, box2):
+    """
+    Checks if there is a horizontal edge present between given boxes.
+    """
+    i = max(box1, box2)
+    return h_vector[box_to_dot(i,state)] != 0
+
+
+def _minimax(state, maximizing_player_id,depth=6):
     """
     Implements a min-max algorithm
-
     Arguments:
       state: The current state node of the game.
       maximizing_player_id: The id of the MAX player. The other player is assumed
@@ -38,18 +160,154 @@ def _minimax(state, maximizing_player_id):
     Returns:
       The optimal value of the sub-game starting in state
     """
-
-    if state.is_terminal():
-        return state.player_return(maximizing_player_id)
-
+    if state.is_terminal() or depth == 0:
+        return calc_heuristic_value(state,maximizing_player_id)
+        
     player = state.current_player()
+
     if player == maximizing_player_id:
         selection = max
     else:
         selection = min
-    values_children = [_minimax(state.child(action), maximizing_player_id) for action in state.legal_actions()]
+    values_children = [_minimax(state.child(action), maximizing_player_id,depth-1) for action in state.legal_actions()]
     return selection(values_children)
 
+
+def calc_heuristic_value(state,maximizing_player_id):
+    """
+    Calculate the heuristic value of a state.
+    Arguments:
+      state: The current state node of the game.
+      maximizing_player_id: The id of the MAX player. The other player is assumed
+        to be MIN.
+    Returns:
+        The heuristic value of the state.
+    """
+    heuristic = 0
+    box_won = 0
+    box_lost = 0
+
+    p = state.current_player()
+    
+    boxes = box_state_for_pvector(pvector_for_tensor(game_state_tensor(state)),state.get_game().get_parameters()['num_cols'])
+
+    # Take box loss and win into account
+    for box in boxes:
+        if p == maximizing_player_id:
+            if box == 1:
+                box_won += 1
+                heuristic += 1
+            elif box == 2:
+                box_lost += 1
+                heuristic -= 1
+        else:
+            if box == 1:
+                box_lost += 1
+                heuristic -= 1
+            elif box == 2:
+                box_won += 1
+                heuristic += 1
+
+    # Take chain even/odd into account
+    if count_chains(state) % 2 == 0 and p == maximizing_player_id:
+        heuristic += 1
+    elif count_chains(state) % 2 != 0 and p != maximizing_player_id:
+        heuristic += 1
+
+    if box_won >= 5:
+        heuristic = np.inf
+    elif box_lost >= 5:
+        heuristic = -np.inf
+
+    if p == maximizing_player_id:
+        return heuristic
+    else:
+        return -1*heuristic
+
+def count_chains(state):
+    """
+    Count the amount of long chains in a state.
+    Arguments:
+      state: The current state node of the game.
+    Returns:
+        The amount of chains in the state.
+    """
+    count_chains = 0
+    chain_list = []
+    
+    num_rows = state.get_game().get_parameters()['num_rows']
+    num_cols = state.get_game().get_parameters()['num_cols']
+
+    for box_num in range(num_cols * num_rows):
+        flag = False
+        for chain in chain_list:
+            if box_num in chain:
+                flag = True
+                break
+        
+        if not flag:
+            chain_list.append([box_num])
+            add_chain(state, chain_list, box_num)
+
+    for chain in chain_list:
+        if len(chain) >= 3:
+            count_chains += 1
+
+    return count_chains
+
+def add_chain(state, chain_list, box_num):
+    """
+    Add chains to the chain_list in the given state.
+    Arguments:
+        state: The current state node of the game.
+        chain_list: The list of chains.
+        box_num: The box number.
+
+    Returns:
+        None
+    """ 
+    num_rows = state.get_game().get_parameters()['num_rows']
+    num_cols = state.get_game().get_parameters()['num_cols']
+    state_tensor = game_state_tensor(state)
+
+    neigh_nums = [box_num - 1, box_num - num_cols, box_num + 1 , box_num + num_cols]
+
+    for i in range(len(neigh_nums)):
+        # Ensure that the neighbour is on the board or if it is in the same row as the current box for the 'left' and 'right' positions.
+        if (
+            neigh_nums[i] < 0 
+            or neigh_nums[i] > num_cols * num_rows 
+            or (i % 2 == 0 and neigh_nums[i] // num_cols != box_num // num_cols)):
+                continue
+        
+        flag = False
+        # Check if the neighbour is already in a chain.
+        for chain in chain_list:
+            if neigh_nums[i] in chain:
+                flag = True
+                break
+
+        # Check if there is a between the current box and a vertical neighbour, if not add the neighbor to the chain and
+        # call the function recursively on the neighbor.
+        if not flag and i % 2 == 0:
+            if not is_vedge_present(state,vvector_for_tensor(state_tensor), box_num, neigh_nums[i]):
+                chain_list[-1].append(neigh_nums[i])
+                add_chain(state, chain_list, neigh_nums[i])
+
+        # Check if there is a between the current box and a horizootal neighbour, if not add the neighbor to the chain and
+        # call the function recursively on the neighbor.
+        if not flag and i % 2 != 0:
+            if not is_hedge_present(state,hvector_for_tensor(state_tensor), box_num, neigh_nums[i]):
+                chain_list[-1].append(neigh_nums[i])
+                add_chain(state, chain_list, neigh_nums[i])
+
+def heuristic_to_win(value):
+    if value > 0:
+        return 1
+    elif value < 0:
+        return 2
+    else:
+        return 0
 
 def minimax_search(game,
                    state=None,
@@ -93,7 +351,8 @@ def minimax_search(game,
     v = _minimax(
         state.clone(),
         maximizing_player_id=maximizing_player_id)
-    return v
+    print(v)
+    return heuristic_to_win(v)
 
 
 def main(_):
